7f0c0976c2f99416155f1449e9070e06c9542f79
zbyszek <knapcio@gmail.com>
2020-11-25 07:47:49 +0100

    removed low performance log

--- src/PixiOverlay.jsx
+++ src/PixiOverlay.jsx
@@ -8,19 +8,9 @@ import 'leaflet-pixi-overlay';
 
 import { useLeafletMap } from 'use-leaflet';
 
+PIXI.settings.FAIL_IF_MAJOR_PERFORMANCE_CAVEAT = false;
 PIXI.utils.skipHello();
 const PIXILoader = PIXI.Loader.shared;
-
-function getDefaultIcon (color) {
-	const svgIcon = `<svg style="-webkit-filter: drop-shadow( 1px 1px 1px rgba(0, 0, 0, .4));filter: drop-shadow( 1px 1px 1px rgba(0, 0, 0, .4));" xmlns="http://www.w3.org/2000/svg" fill="${color}" width="36" height="36" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 6.243 6.377 6.903 8 16.398 1.623-9.495 8-10.155 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.342-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>`;
-	return getEncodedIcon(svgIcon);
-}
-
-function getEncodedIcon(svg) {
-	const decoded = unescape(encodeURIComponent(svg));
-	const base64 = btoa(decoded);
-	return `data:image/svg+xml;base64,${base64}`;
-}
 
 const PixiOverlay = ({
 	markers,
@@ -209,6 +199,17 @@ function openPopup (map, data, extraOptions = {}, isPopup) {
 	}
 
 	return popup;
+}
+
+function getDefaultIcon (color) {
+	const svgIcon = `<svg style="-webkit-filter: drop-shadow( 1px 1px 1px rgba(0, 0, 0, .4));filter: drop-shadow( 1px 1px 1px rgba(0, 0, 0, .4));" xmlns="http://www.w3.org/2000/svg" fill="${color}" width="36" height="36" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 6.243 6.377 6.903 8 16.398 1.623-9.495 8-10.155 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.342-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>`;
+	return getEncodedIcon(svgIcon);
+}
+
+function getEncodedIcon(svg) {
+	const decoded = unescape(encodeURIComponent(svg));
+	const base64 = btoa(decoded);
+	return `data:image/svg+xml;base64,${base64}`;
 }
 
 export default PixiOverlay;
